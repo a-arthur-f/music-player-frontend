29ab363dce42f2e33422ba0d9f9bce57b16b80f1
Arthur Fusco <fuscoarthur1@gmail.com>
2020-06-25 18:34:25 -0300

    Função de avançar e retroceder adicionadas

--- src/main.js
+++ src/main.js
@@ -3,37 +3,78 @@ let play = document.querySelector('.playBtn');
 let avancar = document.querySelector('.avancar');
 let retroceder = document.querySelector('.retroceder');
 let state = 0;
+let index = 0;
+
+let tracks = ['./musics/o_medo_e_a_licao.mp3', './musics/in da club.mp3'];
+
+audio.src = tracks[index];
+
+
 
 const playAudio = () => {
     audio.play();
+    state = 1;
 }
 
 const pauseAudio = () => {
     audio.pause();
+    state = 0;
 }
 
 const avancarAudio = ( ) => {
-    audio.currentTime += 10;
+    audio.currentTime = audio.duration;
 }
 
 const retrocederAudio = () => {
-    audio.currentTime -= 10;
+    if(audio.currentTime > 2)
+        audio.currentTime = 0;
+
+    else {
+        if(index < tracks.length - 1) {
+            ++index;
+            audio.src = tracks[index];
+            audio.load();
+            playAudio();
+        }
+
+        else {
+            --index;
+            audio.src = tracks[index];
+            audio.load();
+            playAudio();
+        }
+    }
+        
+}
+
+const verificaFim = () => {
+    if(audio.ended) {
+        if(index < tracks.length - 1) {
+            ++index;
+            audio.src = tracks[index];
+            audio.load();
+            playAudio();
+        }
+
+        else {
+            --index;
+            audio.src = tracks[index];
+            audio.load();
+            playAudio();
+        }
+    }
 }
 
 play.addEventListener('click', () => {
     if(state === 0) {
         playAudio();
-        state = 1;
     }
     else {
         pauseAudio();
-        state = 0;
     }  
 });
 
-setInterval(() => {
-    console.log(audio.currentTime)
-}, 1000);
+audio.onended = verificaFim;
 
 avancar.addEventListener('click', avancarAudio);
 retroceder.addEventListener('click', retrocederAudio);
